25eecd25cc5f3c5b25cf90f0a4541f67d65fe8c9
wusung <twkoci@gmail.com>
2019-01-05 21:28:31 +0800

    add star icon example

---
+++ src/StarIcon.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Icon from '@material-ui/core/Icon';
+
+class StarIcon extends Component {
+    render() {
+        return (
+            <Icon>star</Icon>
+        );
+    }
+}
+
+export default StarIcon;

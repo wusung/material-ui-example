7f2254369840ecdc374b7945218c0cba84bfbc73
wusung <twkoci@gmail.com>
2019-01-07 00:40:13 +0800

    add SVG Material icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
 import StarIcon from './StarIcon';
+import SvgMaterialIcons from "./SvgMaterialIcons";
 
 class App extends Component {
   render() {
     return (
-        <StarIcon />
+        <Grid container>
+            <Grid item xs={12}>
+                <StarIcon />
+            </Grid>
+            <Grid item xs={12}>
+                <SvgMaterialIcons />
+            </Grid>
+        </Grid>
     );
   }
 }

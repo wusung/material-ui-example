ed7418832e16990538f8dd929fb75f66b34b78b7
wusung <twkoci@gmail.com>
2019-05-15 22:18:41 +0800

    add PopperPopupState

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ import PositionedPopper from './utils/popper/PositionedPopper';
 import NoTransitionPopper from './utils/popper/NoTransitionPopper';
 import FakedReferencePopper from './utils/popper/FakedReferencePopper';
 import RenderPropsPopper from "./utils/popper/RenderPropsPopper";
+import PopperPopupState from './utils/popper/PopperPopupState';
 
 class App extends Component {
   render() {
@@ -186,6 +187,9 @@ class App extends Component {
             </Grid>
             <Grid item xs={12}>
                 <RenderPropsPopper />
+            </Grid>
+            <Grid item xs={12}>
+                <PopperPopupState />
             </Grid>
         </Grid>
     );
